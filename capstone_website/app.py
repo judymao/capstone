b2022b4fe774300dc9097b41519de553e4867bc0
judymao <judymao@users.noreply.github.com>
2020-09-18 12:08:33 -0400

    Update app.py

--- capstone_website/app.py
+++ capstone_website/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello World!"
+    return "Hello World! This is where our capstone website will be stored."
 
 @app.route('/<name>')
 def hello_name(name):
