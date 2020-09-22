f9d70a69008fa2b73434c66490600069c9615e16
judymao <judymao@users.noreply.github.com>
2020-09-22 13:33:58 -0400

    Add login

--- capstone_website/capstone_website/views.py
+++ capstone_website/capstone_website/views.py
@@ -1,5 +1,6 @@
-from flask import render_template, request
+from flask import render_template, request, flash, redirect
 from flask_sqlalchemy import SQLAlchemy
+from .forms import LoginForm
 
 from capstone_website import app
 
@@ -12,6 +13,20 @@ def index():
 @app.route('/about')
 def about():
     return render_template("about.html")
+
+
+@app.route('/register')
+def register():
+    return render_template("registration.html")
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(form.email.data, form.remember_me.data))
+        return redirect('/index')
+    return render_template("login.html", form=form)
 
 
 @app.route('/submit', methods=['POST'])
