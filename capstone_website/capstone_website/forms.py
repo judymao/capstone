6cc962570bcdb2a5e5502b2489d63b17a024758d
judymao <judymao@users.noreply.github.com>
2020-09-23 10:08:00 -0400

    Add login page

--- capstone_website/capstone_website/forms.py
+++ capstone_website/capstone_website/forms.py
@@ -1,10 +1,10 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import InputRequired
 
 
 class LoginForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
+    email = StringField('Email', validators=[InputRequired()])
+    password = PasswordField('Password', validators=[InputRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
--- capstone_website/capstone_website/views.py
+++ capstone_website/capstone_website/views.py
@@ -26,7 +26,7 @@ def login():
     if form.validate_on_submit():
         flash('Login requested for user {}, remember_me={}'.format(form.email.data, form.remember_me.data))
         return redirect('/index')
-    return render_template("login.html", form=form)
+    return render_template("login.html", title="Login", form=form)
 
 
 @app.route('/submit', methods=['POST'])
