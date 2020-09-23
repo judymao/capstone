3e48c410abc8d5a2fee88a46ceeeabc17b7576e3
judymao <judymao@users.noreply.github.com>
2020-09-23 19:09:50 -0400

    Update SQLALCHEMY_DATABASE_URI to read environment variables

--- capstone_website/capstone_website/config.py
+++ capstone_website/capstone_website/config.py
@@ -11,8 +11,10 @@ class Config(object):
 
 class ProductionConfig(Config):
     DEBUG = False
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
 
 class DevelopmentConfig(Config):
     DEVELOPMENT = True
     DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('LOCAL_DB','postgresql://postgres:postgres@127.0.0.1/capstone_website')
