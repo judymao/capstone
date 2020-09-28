8571ea902ef46d893571ccdd960b60abf4ff7229
judymao <judymao@users.noreply.github.com>
2020-09-28 12:00:08 -0400

    Update login/registration, add user page

--- capstone_website/capstone_website/__init__.py
+++ capstone_website/capstone_website/__init__.py
@@ -35,7 +35,7 @@ Bootstrap(app)
 # Initialize login manager
 login_manager = LoginManager()
 login_manager.init_app(app)
-login_manager.login_view = 'login'
+login_manager.login_view = 'auth.login'
 
 
 from .main import main as main_blueprint
--- capstone_website/capstone_website/auth/views.py
+++ capstone_website/capstone_website/auth/views.py
@@ -1,5 +1,6 @@
 from flask import render_template, request, flash, redirect, url_for
 from flask_login import login_user, logout_user, login_required, current_user
+from werkzeug.urls import url_parse
 from .forms import LoginForm, RegisterForm
 from . import auth
 from ..models import User
@@ -12,20 +13,25 @@ def login():
         return redirect(url_for('main.index'))
 
     form = LoginForm()
-    if form.validate_on_submit():
+    if form.validate_on_submit() and request.method == 'POST':
         user = User.query.filter_by(user=form.user.data).first()
         if user is None or not user.verify_password(form.password.data):
             flash('Invalid username or password.')
             return redirect(url_for('auth.login'))
         login_user(user, remember=form.remember_me.data)
-        return redirect(url_for('main.index'))
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('main.index')
+        return redirect(next_page)
     return render_template('auth/login.html', title="Login", form=form)
 
 
 @auth.route('/register', methods=['GET', 'POST'])
 def register():
-    form = RegisterForm()
+    if current_user.is_authenticated:
+        return redirect(url_for('main.index'))
 
+    form = RegisterForm()
     if form.validate_on_submit() and request.method == 'POST':
         # Create a new instance of User
         user = User(user=form.user.data, email=form.email.data, password=form.password.data)
@@ -33,7 +39,8 @@ def register():
         # Save user data into the database
         db.session.add(user)
         db.session.commit()
-        # token = user.generate_confirmation_token()
+
+        flash('Successfully registered!')
         return redirect(url_for('auth.login'))
 
     return render_template('auth/registration.html', title="Register", form=form)
