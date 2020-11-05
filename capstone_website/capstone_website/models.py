0e36662d96c76b7801ed099b1233d72b820dcc5d
mary <mary.bm.wu@gmail.com>
2020-11-05 12:26:11 -0500

    Define three table schemas

--- capstone_website/capstone_website/models.py
+++ capstone_website/capstone_website/models.py
@@ -1,7 +1,14 @@
 from capstone_website import db, login_manager
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+import tiingo
+import pandas_datareader as pdr
 
+tiingo_config = {}
+tiingo_config['session'] = True
+#TODO: API key should be a constant; maybe store in separate file
+tiingo_config['api_key'] = "58245c95b56205705dabecbbfd7e8a346b000bf7" # StockConstants.API
+client = tiingo.TiingoClient(tiingo_config)
 
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
@@ -9,6 +16,9 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user = db.Column(db.String(16), unique=True)
     email = db.Column(db.String(255), unique=True)
+    first_name = db.Column(db.String(255))
+    last_name = db.Column(db.String(255))
+    company = db.Column(db.String(255))
     password_hash = db.Column(db.String(128))
 
     def __repr__(self):
@@ -25,7 +35,63 @@ class User(UserMixin, db.Model):
     def verify_password(self, password):
         return check_password_hash(self.password_hash, password)
 
-db.create_all() # Create tables in db if they do not already exist 
+class Stock(db.Model):
+    __tablename__ = "stocks"
+
+    id = db.Column(db.Integer, primary_key=True)
+    ticker = db.Column(db.String(255), unique=True)
+    date = db.Column(db.Date)
+    open = db.Column(db.Float)
+    high = db.Column(db.Float)
+    low = db.Column(db.Float)
+    close = db.Column(db.Float)
+    volume = db.Column(db.Integer)
+
+    def __repr__(self):
+        return '<Stock %r>' % self.ticker
+
+    def getParams(self, ticker, start_date, end_date):
+        '''
+        Check if ticker already exists in database. If not, query Tiingo
+        '''
+
+        try:
+            data = client.get_dataframe(ticker,
+                                        metric_name="adjClose",
+                                        startDate=start_date,
+                                        endDate=end_date,
+                                        frequency="monthly") #TODO: turn this to variable
+        except tiingo.restclient.RestClientError:
+            print(f"Failed for ticker: {ticker}")
+
+        rets = data.pct_change().dropna()  # create return timeseries
+        rets.columns = [ticker]
+
+        mu = rets.mean()
+        std = rets.std()
+
+
+class Portfolio(db.Model):
+    __tablename__ = "portfolios"
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    # Portfolio metadata
+    protect_portfolio = db.Column(db.String(255))
+    inv_philosophy = db.Column(db.String(255))
+    next_expenditure = db.Column(db.String(255))
+    name = db.Column(db.String(255))
+    time_horizon = db.Column(db.Float)
+    holding_constraint = db.Column(db.Float)
+    trade_size_constraint = db.Column(db.Float)
+
+    # Time-series data
+    date = db.Column(db.Date)
+    assets = db.Column(db.ARRAY(db.String(255)))
+    weights = db.Column(db.ARRAY(db.Float))
+    value = db.Column(db.Integer)
+
 
 db.create_all() # Create tables in the db if they do not already exist
 
