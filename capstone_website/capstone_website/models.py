499554700e4944efc9f8fa770bf8e36050e10482
mary <mary.bm.wu@gmail.com>
2020-11-23 20:35:59 -0500

    Created data extraction process using Tiingo, added to codebase

--- capstone_website/capstone_website/models.py
+++ capstone_website/capstone_website/models.py
@@ -16,7 +16,7 @@ chart_studio.tools.set_credentials_file(username='marycapstone', api_key='lLReEJ
 tiingo_config = {}
 tiingo_config['session'] = True
 # TODO: API key should be a constant; maybe store in separate file
-tiingo_config['api_key'] = "58245c95b56205705dabecbbfd7e8a346b000bf7"  # StockConstants.API
+tiingo_config['api_key'] = "57faeaf57f08c983e03aee6f91ffc72ba2c40a55"  # StockConstants.API
 client = tiingo.TiingoClient(tiingo_config)
 
 
@@ -61,25 +61,85 @@ class Stock(db.Model):
     def __repr__(self):
         return '<Stock %r>' % self.ticker
 
-    def get_tiingo(self, ticker, start_date, end_date):
+    @staticmethod
+    def get_data(tickers, start_date, end_date, freq="D", cols=None):
         '''
         Check if ticker already exists in database. If not, query Tiingo
         '''
 
         try:
-            data = client.get_dataframe(ticker,
-                                        metric_name="adjClose",
-                                        startDate=start_date,
-                                        endDate=end_date,
-                                        frequency="monthly")  # TODO: turn this to variable
-        except tiingo.restclient.RestClientError:
-            print(f"Failed for ticker: {ticker}")
 
-        #TODO: what do I want to return from here?
-        rets = data.pct_change().dropna()  # return timeseries
-        # rets.columns = [ticker]
-        return rets
+            stock_query = Stock.query.filter(
+                Stock.date >= start_date,
+                Stock.date <= end_date,
+                Stock.ticker.in_(tickers)
+            )
 
+            stock_data = pd.read_sql(stock_query.statement, db.session.bind)
+            if stock_data.shape[0]:
+                if cols is not None and isinstance(cols, list):
+                    stock_data = stock_data[cols]
+                #TODO: grouper was giving me errors
+                # stock_data = stock_data.groupby(pd.Grouper(freq=freq)).last().dropna()
+
+            retrieved_tickers = stock_data.ticker.unique().tolist()
+            missing_tickers = [x for x in tickers if x not in retrieved_tickers]
+            tiingo_data = Stock.get_tiingo_data(missing_tickers, start_date, end_date, freq, metric_name=cols)
+            stock_data = stock_data.append(tiingo_data)
+
+        except Exception as e:
+            print(f"Stock:get_data - Ran into Exception: {e}. Retrieving from Tiingo...")
+            stock_data = Stock.get_tiingo_data(tickers, start_date, end_date, freq, metric_name=cols)
+
+        return stock_data
+
+    @staticmethod
+    def get_tiingo_data(tickers, start_date, end_date, freq="D", metric_name=None):
+
+        freq_mapping = {"D" : "daily",
+                        "M": "monthly"}
+
+        tiingo_col = ["adjOpen", "adjHigh", "adjLow", "adjClose", "adjVolume"]
+        col_mapping = {x: x.strip("adj").lower() for x in tiingo_col}
+
+        freq = "D" if freq not in freq_mapping.keys() else freq
+
+        stock_data = pd.DataFrame({})
+        for ticker in tickers:
+            try:
+                if metric_name is not None:
+                    data = client.get_dataframe(ticker,
+                                                metric_name=metric_name,
+                                                startDate=start_date,
+                                                endDate=end_date,
+                                                frequency=freq_mapping[freq])
+                else:
+                    data = client.get_dataframe(ticker,
+                                                startDate=start_date,
+                                                endDate=end_date,
+                                                frequency=freq_mapping[freq])
+
+                data = data[tiingo_col].rename(columns=col_mapping)
+                data["ticker"] = ticker
+                data = data.reset_index()
+                stock_data = stock_data.append(data)
+
+            except tiingo.restclient.RestClientError:
+                print(f"Failed for ticker: {ticker}")
+
+        # Store retrieved stock data to the database
+        if stock_data.shape[0]:
+            # TODO: Grouper giving me issues
+            # stock_data = stock_data.groupby(pd.Grouper(freq=freq)).last().dropna()
+            stocks = [Stock(ticker=stock["ticker"], date=stock["date"],
+                            open=stock["open"], close=stock["close"],
+                            high=stock["high"], low=stock["low"],
+                            volume=stock["volume"]) for stock in stock_data.to_dict(orient="rows")]
+            db.session.add_all(stocks)
+            db.session.commit()
+
+        print(stock_data)
+        return stock_data
 
 class PortfolioInfo(db.Model):
     __tablename__ = "portfolio_info"
@@ -108,20 +168,23 @@ class PortfolioInfo(db.Model):
         # This code is garbage but will be replaced so whatevs I guess
 
         start_date = date(2019, 11, 10)
-        tickers = ["MSFT", "AAPL"]
+        tickers = ["GOOGL", "AAPL"]
         num_assets = len(tickers)
 
-        stock_query = Stock.query.filter(Stock.date >= start_date)
-        stock_data = pd.read_sql(stock_query.statement, db.session.bind)
+        # stock_query = Stock.query.filter(Stock.date >= start_date)
+        # stock_data = pd.read_sql(stock_query.statement, db.session.bind)
+        stock_data = Stock.get_data(tickers=tickers, start_date=start_date, end_date=date.today())
 
         if stock_data.shape[0]:
             # Only get close data and aggregate by date
             # Caution: date formats MIGHT beself different since it's datetime, not date
             stock_data = stock_data[["ticker", "date", "close"]]
             portfolio = pd.DataFrame({"assets": stock_data.groupby("date")["ticker"].unique()}).reset_index()
+            # TODO: currently ignoring dates where inconsistent number of assets
             portfolio.loc[:, "close"] = stock_data.groupby("date")["close"].unique().values
+            portfolio = portfolio[portfolio["assets"].apply(lambda row: len(row)) > 1]
             portfolio["weights"] = [[1/num_assets for i in range(num_assets)] for x in range(portfolio.shape[0])]
-            portfolio.loc[:, "value"] = [np.dot(np.array(portfolio.close[x]), np.array(portfolio.weights[x])) for x in range(portfolio.shape[0])]
+            portfolio.loc[:, "value"] = [np.dot(np.array(portfolio.close.iloc[x]), np.array(portfolio.weights.iloc[x])) for x in range(portfolio.shape[0])]
             portfolio = portfolio.drop("close", axis=1)
             portfolio.loc[:, "user_id"] = self.user_id
             portfolio.loc[:, "portfolio_id"] = self.id
@@ -138,7 +201,7 @@ class PortfolioInfo(db.Model):
             # portfolio_table =
 
             return portfolio_graph_url, [PortfolioData(user_id=p['user_id'], portfolio_id=p['portfolio_id'], date=p['date'],
-                                  assets=p['assets'], weights=p['weights'], value=p['value']) for p in portfolio.to_dict(orient="rows")]
+                                                       assets=p['assets'], weights=p['weights'], value=p['value']) for p in portfolio.to_dict(orient="rows")]
 
 
 
